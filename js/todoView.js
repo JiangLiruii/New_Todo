09a49e5bc9af724197e383847343f10cd61dfb2a
Big_Panda <535904200@qq.com>
2018-04-25 18:55:19 +0800

    update todoView Event including additem,startitem adnd sync items

--- js/todoView.js
+++ js/todoView.js
@@ -1,14 +1,83 @@
 (function start($) {
-  function onEmpty() {
-    $('#todoInput').attr('placeholder', '内容不可为空');
-    $('#todoInput').placeholder.css('color', 'red');
-  }
-  $(document).on('emptyInput', onEmpty);
+  const syncDom = $('#syncDom');
+  const prompt = $('#prompt');
+  const todoInput = $('#todoInput');
+  const content = $('#listcontent');
+  let time;
 
-  function itemUpdate() {}
-  $(document).on('itemUpdate', () => {
-    $('#todoInput').val('');
-    // 更新列表
-  });
-  $(document).on('selectUpdate', itemUpdate);
+  $(document).on('emptyInput', onEmpty);
+  $(document).on('startSync', onStartSync);
+  $(document).on('itemUpdate', itemUpdate);
+  $(document).on('click', '#addButton', onAdd);
+  $(document).on('click', '.delete', onDelete);
+
+  function onAdd() {
+    if (todoInput.val().trim() === '') {
+      onEmpty();
+    } else {
+      $(document).trigger('itemAdd', todoInput.val());
+    }
+  }
+
+  function onDelete(e) {
+    // todo 确认弹窗
+    $(document).trigger('itemDelete', {
+      row: {
+        id: $(e.target).attr('id'),
+        rev: $(e.target).attr('rev'),
+      },
+    });
+  }
+
+  function onEmpty() {
+    if (time) {
+      clearTimeout(time);
+    }
+    prompt.text('内容不可为空').css('color', 'red');
+    time = setTimeout(() => {
+      time = '';
+      prompt.text('');
+    }, 2000);
+  }
+
+
+  function onStartSync() {
+    syncDom.val('syncing');
+    $(document).trigger('firstSyncComplete');
+  }
+
+  function itemUpdate(e, {
+    rows,
+  }) {
+    content.children().remove();
+    let todoLists = '';
+    todoInput.val('');
+    if (rows.length > 0) {
+      rows.forEach((row) => {
+        const {
+          doc: {
+            complete,
+            title,
+            date,
+          },
+        } = row;
+        const todolist = `<tr>
+        <td>
+          <input type='checkbox' class="complete" ${complete ? 'checked' : ''} >
+        </td>
+        <td>
+          ${title}
+        </td>
+        <td>
+          ${date}
+        </td>
+        <td>
+          <button class='delete' id=${row.id} rev=${row.doc._rev}>x</button>
+        </td>
+      </tr>`;
+        todoLists += todolist;
+      });
+    }
+    content.append($(todoLists));
+  }
 }(jQuery));
