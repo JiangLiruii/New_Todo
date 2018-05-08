eaf37f3ed77dd2c362fa2b379a96d621edfed5a8
Big_Panda <535904200@qq.com>
2018-05-08 11:14:45 +0800

    引入IIFE避免全局变量的污染,使用字面量优化事件相关代码.

--- js/todoControl.js
+++ js/todoControl.js
@@ -1,115 +1,116 @@
-const db = new PouchDB('todos');
-const itemUpdate = new CustomEvent('itemUpdate', { detail: {} });
-const itemChanged = new CustomEvent('itemChanged', { detail: {} });
+const itemUpdate = new CustomEvent('itemUpdate', {
+  detail: { data: {} },
+});
 const startSync = new CustomEvent('startSync');
-const itemDeleted = new CustomEvent('itemDeleted', { detail: {} });
-const itemAdded = new CustomEvent('itemAdded', { detail: {} });
+const itemAdded = new CustomEvent('itemAdded', {
+  detail: { data: {} },
+});
+(function startControl(doc) {
+  const db = new PouchDB('todos');
+  doc.addEventListener('onSyncRecieve', sync);
+  doc.addEventListener('itemDelete', onItemDelete);
+  doc.addEventListener('itemChange', onItemDataChange);
+  doc.addEventListener('itemAdd', onitemAdd);
 
-doc.addEventListener('onSyncRecieve', sync);
-doc.addEventListener('itemDelete', onItemDelete);
-doc.addEventListener('itemChange', onItemDataChange);
-doc.addEventListener('itemAdd', onitemAdd);
-
-function sync() {
-  db.allDocs({
-    include_docs: true,
-    descending: true,
-  }, (err, docs) => {
-    const {
-      complete,
-      addDate,
-      finishDate,
-    } = onSyncRecieve.detail;
-    const rowsSend = [];
-
-    docs.rows.forEach((row) => {
-      const dateBoolean = (!addDate || row.doc.date === addDate) &&
-        (!finishDate || row.doc.finishDate === finishDate);
-      if (complete === 'all' && !addDate && !finishDate) {
-        rowsSend.push(row.doc);
-      } else if (complete === 'all' && dateBoolean) {
-        rowsSend.push(row.doc);
-      } else if (row.doc.complete === (complete === 'completed') && dateBoolean) {
-        rowsSend.push(row.doc);
-      }
-    });
-
-    itemUpdate.detail.rows = rowsSend;
-    doc.dispatchEvent(itemUpdate);
-  });
-}
-
-function onitemAdd() {
-  db.put(itemAdd.detail, (err, res) => {
-    if (!err) {
-      itemAdd.detail._rev = res.rev;
+  function sync() {
+    db.allDocs({
+      include_docs: true,
+      descending: true,
+    }, (err, docs) => {
       const {
         complete,
-        title,
-        date,
+        addDate,
         finishDate,
-        _rev,
-        _id,
-      } = itemAdd.detail;
-      itemUpdate.detail.rows.unshift({
-        complete,
-        title,
-        date,
-        finishDate,
-        _rev,
-        _id,
-      });
-      doc.dispatchEvent(itemAdded);
-    } else {
-      console.error('something error', err);
-    }
-  });
-}
-
-function onItemDelete() {
-  const rows = itemUpdate.detail.rows;
-  db.remove(itemDelete.detail.id, itemDelete.detail.rev)
-    .then(() => {
-      itemDeleted.detail.target = itemDelete.detail.source;
-      rows.forEach((row) => {
-        if (row._id === itemDelete.detail.id) {
-          rows.splice(rows.indexOf(row), 1);
+      } = onSyncRecieve.detail.data;
+      const rowsSend = [];
+      docs.rows.forEach((row) => {
+        const dateBoolean = (!addDate || row.doc.date === addDate) &&
+                    (!finishDate || row.doc.finishDate === finishDate);
+        if (complete === 'all' && !addDate && !finishDate) {
+          rowsSend.push(row.doc);
+        } else if (complete === 'all' && dateBoolean) {
+          rowsSend.push(row.doc);
+        } else if (row.doc.complete === (complete === 'completed') && dateBoolean) {
+          rowsSend.push(row.doc);
         }
       });
-      doc.dispatchEvent(itemDeleted);
+      itemUpdate.detail.rows = rowsSend;
+      doc.dispatchEvent(itemUpdate);
     });
-}
+  }
 
-function onItemDataChange() {
-  const {
-    _id,
-    _rev,
-    date,
-    title,
-    complete,
-    finishDate,
-  } = itemChange.detail;
-  db.put({
-    _id,
-    _rev,
-    date,
-    title,
-    complete,
-    finishDate,
-  }).then((docs) => {
-    itemChanged.detail.rev = docs.rev;
-    itemUpdate.detail.rows.forEach((row) => {
-      if (row._id === _id) {
-        row.date = date;
-        row.title = title;
-        row.finishDate = finishDate;
-        row._rev = docs.rev;
-        row.complete = complete;
+  function onitemAdd() {
+    const data = itemAdd.detail.data;
+    db.put(data, (err, res) => {
+      if (!err) {
+        data._rev = res.rev;
+        const {
+          complete,
+          title,
+          date,
+          finishDate,
+          _rev,
+          _id,
+        } = data;
+        itemUpdate.detail.rows.unshift({
+          complete,
+          title,
+          date,
+          finishDate,
+          _rev,
+          _id,
+        });
+        doc.dispatchEvent(itemAdded);
+      } else {
+        console.error('something error', err);
       }
     });
-    doc.dispatchEvent(itemChanged);
-  });
-}
-doc.onload = (() => {
-  doc.dispatchEvent(startSync);
-})();
+  }
+
+  function onItemDelete() {
+    const data = itemUpdate.detail.data;
+    const rows = itemUpdate.detail.rows;
+    db.remove(data.id, data.rev)
+      .then(() => {
+        rows.forEach((row) => {
+          if (row._id === data.id) {
+            rows.splice(rows.indexOf(row), 1);
+          }
+        });
+        doc.dispatchEvent(itemUpdate);
+      });
+  }
+
+  function onItemDataChange() {
+    const {
+      _id,
+      _rev,
+      date,
+      title,
+      complete,
+      finishDate,
+    } = itemChange.detail.data;
+    db.put({
+      _id,
+      _rev,
+      date,
+      title,
+      complete,
+      finishDate,
+    }).then((docs) => {
+      itemUpdate.detail.rows.forEach((row) => {
+        if (row._id === _id) {
+          row.date = date;
+          row.title = title;
+          row.finishDate = finishDate;
+          row._rev = docs.rev;
+          row.complete = complete;
+        }
+      });
+      doc.dispatchEvent(itemUpdate);
+    });
+  }
+  doc.onload = (() => {
+    doc.dispatchEvent(startSync);
+  })();
+}(document));
--- js/todoView.js
+++ js/todoView.js
@@ -1,222 +1,224 @@
-const doc = document;
-const syncDom = doc.getElementById('syncDom');
-const prompt = doc.getElementById('prompt');
-const todoInput = doc.getElementById('todoInput');
-const content = doc.getElementById('content');
-const footer = doc.getElementsByTagName('footer')[0];
-// 用于计时
-let time;
-// 用于升降序
-let descending = false;
+const itemAdd = new CustomEvent('itemAdd', {
+  detail: { data: {} },
+});
+const itemDelete = new CustomEvent('itemDelete', {
+  detail: { data: {} },
+});
+const onSyncRecieve = new CustomEvent('onSyncRecieve', {
+  detail: { data: {} },
+});
+const itemChange = new CustomEvent('itemChange', {
+  detail: { data: {} },
+});
+(function startView(doc) {
+  const syncDom = doc.getElementById('syncDom');
+  const prompt = doc.getElementById('prompt');
+  const todoInput = doc.getElementById('todoInput');
+  const content = doc.getElementById('content');
+  const footer = doc.getElementsByTagName('footer')[0];
+  // 用于计时
+  let time;
+  // 用于升降序
+  let descending = false;
+  doc.addEventListener('startSync', onStartSync);
+  doc.addEventListener('itemUpdate', onitemUpdate);
+  doc.addEventListener('click', onClickFunc);
+  doc.addEventListener('change', onChangeFunc);
 
-const itemAdd = new CustomEvent('itemAdd', { detail: {} });
-const itemDelete = new CustomEvent('itemDelete', { detail: {} });
-const onSyncRecieve = new CustomEvent('onSyncRecieve', { detail: {} });
-const itemChange = new CustomEvent('itemChange', { detail: {} });
-
-doc.addEventListener('startSync', onStartSync);
-doc.addEventListener('itemUpdate', onitemUpdate);
-doc.addEventListener('click', onClickFunc);
-doc.addEventListener('change', onChangeFunc);
-doc.addEventListener('itemChanged', onItemChanged);
-doc.addEventListener('itemDeleted', onItemDeleted);
-doc.addEventListener('itemAdded', onItemAdded);
-
-function onChangeFunc(e) {
-  if (e.target.parentNode.id === 'filter') {
-    onFilterChange(e);
-  } else if (e.target.parentNode.className === 'contentWrap') {
-    onItemChange(e);
-  }
-}
-
-function onClickFunc(e) {
-  if (e.target.parentNode.className === 'itemDelete') {
-    onDelete(e.target);
-  } else if (e.target.id === 'addButton') {
-    onAdd();
-  } else if (e.target.className === 'pages') {
-    onPageClick(e.target);
-  } else if (e.target.parentNode.id === 'title') {
-    onTitleClick(e);
-  }
-}
-
-function onFilterChange(e) {
-  if (e.target.id === 'filterAdd') {
-    onSyncRecieve.detail.addDate = e.target.value;
-  } else if (e.target.id === 'filterComplete') {
-    onSyncRecieve.detail.finishDate = e.target.value;
-  } else if (e.target.id === 'completeSelect') {
-    onSyncRecieve.detail.complete = e.target.value;
-  }
-  doc.dispatchEvent(onSyncRecieve);
-}
-
-function onPageClick(element) {
-  let nextPage = +element.getAttribute('page');
-  switch (nextPage) {
-    case 0:
-      nextPage = 1;
-      break;
-    case -1:
-      nextPage = Math.ceil(itemUpdate.detail.rows.length / 10);
-      break;
-    default:
-      break;
-  }
-
-  itemUpdate.detail.currentPage = nextPage;
-  onitemUpdate();
-}
-
-function onTitleClick(e) {
-  // 根据e.target.class来排序
-  let className = `${e.target.className.replace('item', '')}`;
-  className = className[0].toLowerCase() + className.substr(1);
-
-  const sortedContent = itemUpdate.detail.rows;
-  if (!descending) {
-    sortedContent.sort((itemA, itemB) => {
-      console.log(itemA[className], itemB[className], itemA[className] > itemB[className]);
-      return itemA[className] < itemB[className];
-    });
-    descending = true;
-  } else {
-    sortedContent.sort((itemA, itemB) => {
-      console.log(itemA[className], itemB[className], itemA[className] < itemB[className]);
-      return itemA[className] > itemB[className];
-    });
-    descending = false;
-  }
-  itemUpdate.detail.rows = sortedContent;
-  doc.dispatchEvent(itemUpdate);
-}
-
-function onAdd() {
-  const finishDate = doc.getElementById('finishDate').value;
-  const date = new Date();
-  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
-  const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
-  const newDate = `${date.getFullYear()}-${month}-${day}`;
-  if (todoInput.value.trim() === '') {
-    onEmpty();
-  } else {
-    itemAdd.detail._id = date.getTime().toString();
-    itemAdd.detail.title = todoInput.value.trim();
-    itemAdd.detail.date = newDate;
-    itemAdd.detail.finishDate = finishDate || newDate;
-    itemAdd.detail.complete = false;
-    itemAdd.detail._rev = null;
-    doc.dispatchEvent(itemAdd);
-  }
-}
-function onItemAdded() {
-  onitemUpdate();
-}
-
-function onDelete(ele) {
-  // item 确认弹窗
-  const item = ele.parentNode.parentNode;
-  itemDelete.detail.id = item.getAttribute('_id');
-  itemDelete.detail.rev = item.getAttribute('_rev');
-  itemDelete.detail.source = item;
-  doc.dispatchEvent(itemDelete);
-}
-function onItemDeleted() {
-  onitemUpdate();
-}
-
-function onItemChange(e) {
-  const item = e.target.parentNode;
-  // 单击完成触发事件
-  if (e.target.className === 'itemComplete') {
-    itemChange.detail.complete = !item.children[0].hasAttribute('checked');
-    item.setAttribute('_complete', itemChange.detail.complete);
-  } else {
-    itemChange.detail.complete = item.children[0].hasAttribute('checked');
-  }
-  itemChange.detail.target = item;
-  itemChange.detail._rev = item.getAttribute('_rev');
-  itemChange.detail._id = item.getAttribute('_id');
-  itemChange.detail.title = item.children[1].value;
-  itemChange.detail.date = item.children[2].value;
-  itemChange.detail.finishDate = item.children[3].value;
-  doc.dispatchEvent(itemChange);
-}
-
-function onItemChanged() {
-  onitemUpdate();
-}
-
-function onEmpty() {
-  if (time) {
-    clearTimeout(time);
-  }
-  prompt.innerText = '内容不可为空';
-  prompt.style.color = 'red';
-  time = setTimeout(() => {
-    time = '';
-    prompt.innerText = '';
-  }, 2000);
-}
-
-
-function onStartSync() {
-  syncDom.innerText = 'SYNCING';
-  onSyncRecieve.detail.complete = 'all';
-  onSyncRecieve.detail.addDate = '';
-  onSyncRecieve.detail.finishDate = '';
-  doc.dispatchEvent(onSyncRecieve);
-}
-
-function onitemUpdate() {
-  const pages = Math.ceil(itemUpdate.detail.rows.length / 10);
-  const currentPage = itemUpdate.detail.currentPage || 1;
-  const rows = itemUpdate.detail.rows.slice((currentPage - 1) * 10, currentPage * 10);
-  todoInput.value = '';
-  let todoLists = '';
-  content.innerHTML = '';
-  doc.getElementById('finishDate').value = '';
-  if (rows.length > 0) {
-    rows.forEach((row) => {
-      todoLists += itemDomCreate(row);
-    });
-  }
-  content.innerHTML = todoLists;
-  paginate(pages, currentPage);
-  syncDom.innerText = 'SYNCED';
-}
-
-function paginate(pages, currentPage) {
-  const pagination = doc.getElementById('pages');
-  let paginationHTML = '<br><span page="0" class="pages"><<</span>';
-  for (let i = 1; i <= pages; i += 1) {
-    if (i === currentPage) {
-      paginationHTML += `<span page="${i}" class="pages active">${i}</span>`;
-    } else {
-      paginationHTML += `<span page="${i}" class="pages">${i}</span>`;
+  function onChangeFunc(e) {
+    if (e.target.parentNode.id === 'filter') {
+      onFilterChange(e);
+    } else if (e.target.parentNode.className === 'contentWrap') {
+      onItemChange(e);
     }
   }
-  paginationHTML += '<span page="-1" class="pages">>></span>';
-  pagination.innerHTML = paginationHTML;
-  footer.insertBefore(pagination, syncDom);
-}
 
-function itemDomCreate(row) {
-  const {
-    complete,
-    title,
-    date,
-    finishDate,
-    _rev,
-    _id,
-  } = row;
-  return `<div class="contentWrap"  _complete=${complete} _title=${title} _date=${date} _finishDate=${finishDate}  _id=${_id} _rev=${_rev}>
-  <input class="itemComplete" type='checkbox' ${complete ? 'checked' : ''} >
-  <input class="itemTitle" value=${title}>
-  <input type="date" class="itemDate" value=${date}>
-  <input type="date" class="itemFinishDate" value=${finishDate}>
-  <span class='itemDelete' value=><button class='itemDelete'>x</button></span>
-</div>`;
-}
+  function onClickFunc(e) {
+    if (e.target.parentNode.className === 'itemDelete') {
+      onDelete(e.target);
+    } else if (e.target.id === 'addButton') {
+      onAdd();
+    } else if (e.target.className === 'pages') {
+      onPageClick(e.target);
+    } else if (e.target.parentNode.id === 'title') {
+      onTitleClick(e);
+    }
+  }
+
+  function onFilterChange(e) {
+    const data = onSyncRecieve.detail.data;
+    if (e.target.id === 'filterAdd') {
+      data.addDate = e.target.value;
+    } else if (e.target.id === 'filterComplete') {
+      data.finishDate = e.target.value;
+    } else if (e.target.id === 'completeSelect') {
+      data.complete = e.target.value;
+    }
+    syncDom.innerText = 'SYNCING';
+    doc.dispatchEvent(onSyncRecieve);
+  }
+
+  function onPageClick(element) {
+    let nextPage = +element.getAttribute('page');
+    switch (nextPage) {
+      case 0:
+        nextPage = 1;
+        break;
+      case -1:
+        nextPage = Math.ceil(itemUpdate.detail.rows.length / 10);
+        break;
+      default:
+        break;
+    }
+    itemUpdate.detail.currentPage = nextPage;
+    onitemUpdate();
+  }
+
+  function onTitleClick(e) {
+    // 根据e.target.class来排序
+    let className = `${e.target.className.replace('item', '')}`;
+    className = className[0].toLowerCase() + className.substr(1);
+
+    const sortedContent = itemUpdate.detail.rows;
+    if (!descending) {
+      sortedContent.sort((itemA, itemB) => {
+        console.log(itemA[className], itemB[className], itemA[className] > itemB[className]);
+        return itemA[className] < itemB[className];
+      });
+      descending = true;
+    } else {
+      sortedContent.sort((itemA, itemB) => {
+        console.log(itemA[className], itemB[className], itemA[className] < itemB[className]);
+        return itemA[className] > itemB[className];
+      });
+      descending = false;
+    }
+    itemUpdate.detail.rows = sortedContent;
+    doc.dispatchEvent(itemUpdate);
+  }
+
+  function onAdd() {
+    const finishDate = doc.getElementById('finishDate').value;
+    const date = new Date();
+    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+    const newDate = `${date.getFullYear()}-${month}-${day}`;
+    if (todoInput.value.trim() === '') {
+      onEmpty();
+    } else {
+      itemAdd.detail.data = {
+        _id: date.getTime().toString(),
+        _rev: null,
+        title: todoInput.value.trim(),
+        date: newDate,
+        finishDate: finishDate || newDate,
+        complete: false,
+      };
+      doc.dispatchEvent(itemAdd);
+    }
+  }
+
+  function onDelete(ele) {
+    // item 确认弹窗
+    const item = ele.parentNode.parentNode;
+    itemDelete.detail.data = {
+      id: item.getAttribute('_id'),
+      rev: item.getAttribute('_rev'),
+    };
+    doc.dispatchEvent(itemDelete);
+  }
+
+  function onItemChange(e) {
+    const item = e.target.parentNode;
+    let complete = '';
+    // 单击完成触发事件
+    if (e.target.className === 'itemComplete') {
+      complete = !item.children[0].hasAttribute('checked');
+      item.setAttribute('_complete', itemChange.detail.complete);
+    } else {
+      complete = item.children[0].hasAttribute('checked');
+    }
+    itemchange.data = {
+      complete,
+      _rev: item.getAttribute('_rev'),
+      _id: item.getAttribute('_id'),
+      title: item.children[1].value,
+      date: item.children[2].value,
+      finishDate: item.children[3].value,
+    };
+    doc.dispatchEvent(itemChange);
+  }
+
+  function onEmpty() {
+    if (time) {
+      clearTimeout(time);
+    }
+    prompt.innerText = '内容不可为空';
+    prompt.style.color = 'red';
+    time = setTimeout(() => {
+      time = '';
+      prompt.innerText = '';
+    }, 2000);
+  }
+
+
+  function onStartSync() {
+    const data = onSyncRecieve.detail.data;
+    syncDom.innerText = 'SYNCING';
+    data.complete = 'all';
+    data.addDate = '';
+    data.finishDate = '';
+    doc.dispatchEvent(onSyncRecieve);
+  }
+
+  function onitemUpdate() {
+    const pages = Math.ceil(itemUpdate.detail.rows.length / 10);
+    const currentPage = itemUpdate.detail.currentPage || 1;
+    const rows = itemUpdate.detail.rows.slice((currentPage - 1) * 10, currentPage * 10);
+    todoInput.value = '';
+    let todoLists = '';
+    content.innerHTML = '';
+    doc.getElementById('finishDate').value = '';
+    if (rows.length > 0) {
+      rows.forEach((row) => {
+        todoLists += itemDomCreate(row);
+      });
+    }
+    content.innerHTML = todoLists;
+    paginate(pages, currentPage);
+    syncDom.innerText = 'SYNCED';
+  }
+
+  function paginate(pages, currentPage) {
+    const pagination = doc.getElementById('pages');
+    let paginationHTML = '<br><span page="0" class="pages"><<</span>';
+    for (let i = 1; i <= pages; i += 1) {
+      if (i === currentPage) {
+        paginationHTML += `<span page="${i}" class="pages active">${i}</span>`;
+      } else {
+        paginationHTML += `<span page="${i}" class="pages">${i}</span>`;
+      }
+    }
+    paginationHTML += '<span page="-1" class="pages">>></span>';
+    pagination.innerHTML = paginationHTML;
+    footer.insertBefore(pagination, syncDom);
+  }
+
+  function itemDomCreate(row) {
+    const {
+      complete,
+      title,
+      date,
+      finishDate,
+      _rev,
+      _id,
+    } = row;
+    return `<div class="contentWrap"  _complete=${complete} _title=${title} _date=${date} _finishDate=${finishDate}  _id=${_id} _rev=${_rev}>
+    <input class="itemComplete" type='checkbox' ${complete ? 'checked' : ''} >
+    <input class="itemTitle" value=${title}>
+    <input type="date" class="itemDate" value=${date}>
+    <input type="date" class="itemFinishDate" value=${finishDate}>
+    <span class='itemDelete' value=><button class='itemDelete'>x</button></span>
+  </div>`;
+  }
+}(document));
