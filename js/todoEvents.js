2d8ea2bd2f1616e9b46afaf719542814b4a946db
JiangLirui <jianglirui@codemao.cn>
2018-05-16 10:52:15 +0800

    更新为ES6的写法

--- js/todoEvents.js
+++ js/todoEvents.js
@@ -4,51 +4,53 @@
  *
  * @class TodoEvent
  *
- * @constructor handlers,rows
+ * @constructor
  *
  */
-function TodoEvent() {
-  this.handlers = {};
-  this.rows = [];
-}
-TodoEvent.prototype.subscribe = function (type, listener) {
-  if (!(type in this.handlers)) {
-    this.handlers[type] = [];
+class TodoEvent {
+  constructor() {
+    this.handlers = {};
+    this.rows = [];
   }
-  this.handlers[type].push(listener);
-};
-TodoEvent.prototype.off = function (type, listener) {
-  let i,
-    position = -1;
-  const list = this.handlers[type],
-    length = this.handlers[type].length;
-  for (i = length - 1; i >= 0; i -= 1) {
-    if (list[i] === listener) {
-      position = i;
-      break;
+  subscribe(type, listener) {
+    if (!(type in this.handlers)) {
+      this.handlers[type] = [];
+    }
+    this.handlers[type].push(listener);
+  }
+  off(type, listener) {
+    let i,
+      position = -1;
+    const list = this.handlers[type],
+      length = this.handlers[type].length;
+    for (i = length - 1; i >= 0; i -= 1) {
+      if (list[i] === listener) {
+        position = i;
+        break;
+      }
+    }
+    if (position === -1) {
+      return;
+    }
+    if (length === 1) {
+      delete this.handlers[type];
+    } else {
+      this.handlers[type].splice(position, 1);
     }
   }
-  if (position === -1) {
-    return;
+  publish(type, ...thisArgs) {
+    const list = this.handlers[type],
+      length = this.handlers[type].length;
+    let i;
+    for (i = length - 1; i >= 0; i -= 1) {
+      list[i].apply(this, thisArgs);
+    }
   }
-  if (length === 1) {
-    delete this.handlers[type];
-  } else {
-    this.handlers[type].splice(position, 1);
+  getTodoRows() {
+    return this.rows;
   }
-};
-TodoEvent.prototype.publish = function (type, ...thisArgs) {
-  const list = this.handlers[type],
-    length = this.handlers[type].length;
-  let i;
-  for (i = length - 1; i >= 0; i -= 1) {
-    list[i].apply(this, thisArgs);
+  setTodoRows(rows) {
+    this.rows = rows;
   }
-};
-TodoEvent.prototype.getTodoRows = function () {
-  return this.rows;
-};
-TodoEvent.prototype.setTodoRows = function (rows) {
-  this.rows = rows;
-};
+}
 export default new TodoEvent();
