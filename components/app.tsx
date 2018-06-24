cdc0144062012e1f5b4d994e75b5612f605fc0c6
Big_Panda <lorry.jiang@hotmail.com>
2018-06-24 15:51:28 +0800

    update for context.provider and conumption

--- components/app.tsx
+++ components/app.tsx
@@ -6,33 +6,33 @@ import PropTypes from '../node_modules/prop-types/index';
 import AddItem from './add_items';
 import ItemFilter from './item_filter';
 import { AddItemStates } from './add_items';
-import ItemList,{ItemDetail} from './item_list';
+import ItemList, { ItemDetail } from './item_list';
 import PaginationComponents from './pagination';
 
 declare const PouchDB;
 
 interface AppState {
-    pageTodoItems:Array<ItemDetail>,
-    current_page:number,
-    filterData:filterData
+    pageTodoItems: Array<ItemDetail>,
+    current_page: number,
+    filterData: filterData
 }
 
 export interface filterData {
-    complete:string,
-    addDate:string,
-    finishDate:string
+    complete: string,
+    addDate: string,
+    finishDate: string
 }
 
-export interface AppContext {
-    itemChange:Function,
-    itemDelete:Function,
-    titleClick:Function,
-}
+export const dbHandles = React.createContext({
+    itemChange: () => { },
+    itemDelete: (id, rev) => { },
+    titleClick: () => { },
+})
 
 export class App extends React.Component<{}, AppState> {
-    perPageNumber:number; // 每页展示多少待办项
-    filterItems:Array<ItemDetail>;
-    allTodoItems:Array<ItemDetail>;
+    perPageNumber: number; // 每页展示多少待办项
+    filterItems: Array<ItemDetail>;
+    allTodoItems: Array<ItemDetail>;
     private db; // 保存数据库
     private descending = true; // 降序排列
 
@@ -41,49 +41,43 @@ export class App extends React.Component<{}, AppState> {
         this.state = {
             pageTodoItems: [],
             current_page: 1,
-            filterData:{complete:'all',addDate:'',finishDate:''}
+            filterData: { complete: 'all', addDate: '', finishDate: '' }
         }
         this.perPageNumber = 6;
         this.allTodoItems = [];
         this.filterItems = [];
     }
-    
-    // 使用context与孙子组件通信
-    static childContextTypes = {
-        itemChange:PropTypes.func,
-        itemDelete:PropTypes.func,
-        titleClick:PropTypes.func,
-    };
-    
-    getChildContext() {
-        return {
-            itemChange:this.itemChange.bind(this),
-            itemDelete:this.itemDelete.bind(this),
-            titleClick:this.titleClick.bind(this),
-        }
-    }
+
     componentWillMount(): void {
         this.db = PouchDB('todos') || new PouchDB('todos');
-        this.itemSync();       
+        this.itemSync();
     }
     render() {
         let pages = Math.ceil(this.allTodoItems.length / this.perPageNumber);
-        return (<div id='react_root'>
-            <AddItem itemAdd={(item,cb) => { this.itemAdd(item,cb) }} />
-            <ItemFilter itemFilter={this.itemFilter.bind(this)}/>
-            <ItemList list={this.state.pageTodoItems} />
-            <PaginationComponents pages={pages} current_page={this.state.current_page} onPageClick={this.onPageClick.bind(this)}/>
-        </div>)
+        return (
+            <div id='react_root'>
+                <dbHandles.Provider value={{
+                    itemChange: this.itemChange.bind(this),
+                    itemDelete: this.itemDelete.bind(this),
+                    titleClick: this.titleClick.bind(this),
+                }}>
+                    <AddItem itemAdd={(item, cb) => { this.itemAdd(item, cb) }} />
+                    <ItemFilter itemFilter={this.itemFilter.bind(this)} />
+                    <ItemList list={this.state.pageTodoItems} />
+                    <PaginationComponents pages={pages} current_page={this.state.current_page} onPageClick={this.onPageClick.bind(this)} />
+                </dbHandles.Provider>
+            </div>
+        )
     }
     /**
      * pagination组件中的点击响应
      * @param click_page 点击的页数
      */
     private onPageClick(click_page) {
-        console.log(click_page,this.state.pageTodoItems);
-        
-        const itemsStart:number = (click_page - 1) * this.perPageNumber;
-        const itemsStop:number = Math.min((click_page) * this.perPageNumber, this.filterItems.length)
+        console.log(click_page, this.state.pageTodoItems);
+
+        const itemsStart: number = (click_page - 1) * this.perPageNumber;
+        const itemsStop: number = Math.min((click_page) * this.perPageNumber, this.filterItems.length)
         this.setState({
             current_page: click_page,
             pageTodoItems: this.filterItems.slice(itemsStart, itemsStop),
@@ -94,9 +88,9 @@ export class App extends React.Component<{}, AppState> {
      * @param item 点击+按钮添加的item
      * @param cb callback 将输入框置为空
      */
-    private itemAdd(item:AddItemStates,cb:Function) {
+    private itemAdd(item: AddItemStates, cb: Function) {
         this.db.put(item).then((res) => {
-            this.filterItems.unshift(Object.assign(item,{_rev:res.rev}));
+            this.filterItems.unshift(Object.assign(item, { _rev: res.rev }));
             this.setPageItems(this.filterItems);
             cb();
         }).catch((err) => { console.log(err) })
@@ -105,11 +99,11 @@ export class App extends React.Component<{}, AppState> {
      * 待办同步函数，在需要与数据库整体同步刷新时调用
      */
     private itemSync() {
-        this.db.allDocs({ include_docs: true, descending: this.descending,},(err,res) => {
-            if(err) {
+        this.db.allDocs({ include_docs: true, descending: this.descending, }, (err, res) => {
+            if (err) {
                 return console.error(err);
             }
-            const items = res.rows.map((row)=>row.doc)
+            const items = res.rows.map((row) => row.doc)
             this.filterItems = items;
             this.allTodoItems = items;
             this.setPageItems(items);
@@ -120,24 +114,24 @@ export class App extends React.Component<{}, AppState> {
      * @param items filterItems
      */
     private setPageItems(items) {
-        const itemStart:number = (this.state.current_page - 1) * this.perPageNumber;
-        const itemStop:number = Math.min(this.state.current_page * this.perPageNumber, items.length)
+        const itemStart: number = (this.state.current_page - 1) * this.perPageNumber;
+        const itemStop: number = Math.min(this.state.current_page * this.perPageNumber, items.length)
         this.setState({
-            pageTodoItems:items.slice(itemStart, itemStop),
+            pageTodoItems: items.slice(itemStart, itemStop),
         })
     }
     /**
      * 当待办项改变时
      * @param changeItem 改变的待办项
      */
-    private itemChange(changeItem:any) {
+    private itemChange(changeItem: any) {
         this.db.put(changeItem).then((docs) => {
             const pageTodoItems = this.state.pageTodoItems
-            pageTodoItems.map((item)=>{
+            pageTodoItems.map((item) => {
                 if (item._id === changeItem._id) {
                     item._rev = docs.rev;
                 }
-                return item; 
+                return item;
             })
             this.setState({
                 pageTodoItems
@@ -149,8 +143,8 @@ export class App extends React.Component<{}, AppState> {
      * @param itemId 待办项id
      * @param itemRev 待办项rev
      */
-    private itemDelete(itemId,itemRev) {
-        this.db.remove(itemId,itemRev).then((docs)=>{
+    private itemDelete(itemId, itemRev) {
+        this.db.remove(itemId, itemRev).then((docs) => {
             this.allTodoItems = this.allTodoItems.filter((item) => item._id !== itemId)
             this.filterItems = this.filterItems.filter((item) => item._id !== itemId)
             this.setPageItems(this.filterItems);
@@ -162,16 +156,16 @@ export class App extends React.Component<{}, AppState> {
      */
     private itemFilter(e) {
         const filterData = {
-            complete:e.currentTarget.children[1].value,
-            addDate:e.currentTarget.children[4].value,
-            finishDate:e.currentTarget.children[6].value,
+            complete: e.currentTarget.children[1].value,
+            addDate: e.currentTarget.children[4].value,
+            finishDate: e.currentTarget.children[6].value,
         }
         const oldState = this.allTodoItems;
         let newState = []
-        for(let i = 0, n = oldState.length; i < n; i += 1) {
-            if((!filterData.addDate || oldState[i].addDate === filterData.addDate) && 
-               (filterData.complete === 'all' || (oldState[i].complete === (filterData.complete === 'completed'))) && 
-               (!filterData.finishDate || oldState[i].finishDate === filterData.finishDate)) {
+        for (let i = 0, n = oldState.length; i < n; i += 1) {
+            if ((!filterData.addDate || oldState[i].addDate === filterData.addDate) &&
+                (filterData.complete === 'all' || (oldState[i].complete === (filterData.complete === 'completed'))) &&
+                (!filterData.finishDate || oldState[i].finishDate === filterData.finishDate)) {
                 newState.push(oldState[i])
             }
         }
@@ -185,18 +179,19 @@ export class App extends React.Component<{}, AppState> {
     private titleClick(e) {
         let className = `${e.target.className.replace('item', '')}`;
         className = className[0].toLowerCase() + className.substr(1);
-      
+
         let sortedItems = this.allTodoItems;
         if (!this.descending) {
-          sortedItems.sort((itemA, itemB) => +(itemA[className] > itemB[className]));
-          this.descending = true;
+            sortedItems.sort((itemA, itemB) => +(itemA[className] > itemB[className]));
+            this.descending = true;
         } else {
-          sortedItems.sort((itemA, itemB) => +(itemB[className] > itemA[className]));
-          this.descending = false;
+            sortedItems.sort((itemA, itemB) => +(itemB[className] > itemA[className]));
+            this.descending = false;
         }
         this.setPageItems(sortedItems);
     }
 }
+
 ReactDOM.render(
     <App />, document.getElementById('root')
 );
--- components/item.tsx
+++ components/item.tsx
@@ -1,42 +1,40 @@
 import * as React from 'react'
-import {AppContext} from './app'
-import PropTypes from '../node_modules/prop-types/index';
+import { dbHandles } from "./app";
 
 
 interface ItemProp {
-  item:{complete:boolean,detail:string,addDate:string,finishDate:string,_id:string,_rev:string},
+  item: { complete: boolean, detail: string, addDate: string, finishDate: string, _id: string, _rev: string },
 }
 
 interface ItemState {
-  detail:string,
+  detail: string,
 }
 
 export default class Item extends React.Component<ItemProp, ItemState> {
-  context:AppContext;
   constructor(props: ItemProp) {
     super(props);
     console.log(this.props);
-    
+
     this.state = {
       detail: this.props.item.detail,
     }
   }
 
-  static contextTypes= {
-    itemChange:PropTypes.func,
-    itemDelete:PropTypes.func,
-};;
-
-
   render() {
     const item = this.props.item;
-    return (<div className="contentWrap" >
-      <span className="complete"><input type='checkbox' checked={item.complete} onChange={this.onDataChange.bind(this)} /></span>
-      <span className="detail"><input value={this.state.detail} onChange={this.onChange.bind(this)}onBlur={this.onDataChange.bind(this)} /></span>
-      <span className="addDate">{item.addDate}</span>
-      <span className="finishDate"><input type="date" value={item.finishDate} onChange={this.onDataChange.bind(this)} /></span>
-      <span className='itemDelete'><button onClick={()=>this.context.itemDelete(item._id,item._rev)}>x</button></span>
-    </div>)
+    return (
+      <dbHandles.Consumer>
+        {({ itemChange, itemDelete }) => (
+          <div className="contentWrap" >
+            <span className="complete"><input type='checkbox' checked={item.complete} onChange={this.onDataChange.bind(this, itemChange)} /></span>
+            <span className="detail"><input value={this.state.detail} onChange={this.onChange.bind(this)} onBlur={this.onDataChange.bind(this)} /></span>
+            <span className="addDate">{item.addDate}</span>
+            <span className="finishDate"><input type="date" value={item.finishDate} onChange={this.onDataChange.bind(this)} /></span>
+            <span className='itemDelete'><button onClick={() => itemDelete(item._id, item._rev)}>x</button></span>
+          </div>
+        )}
+      </dbHandles.Consumer>
+    )
   }
   /**
    * 使input组件可控，且减少数据库读取操作，只在光标移出时进行更新。
@@ -51,15 +49,15 @@ export default class Item extends React.Component<ItemProp, ItemState> {
    * 数据更新时调用
    * @param e changevent
    */
-  private onDataChange (e) {
+  private onDataChange(e, itemChange) {
     const key = e.target.parentNode.className;
     const item = this.props.item;
-    let value = e.target.value; 
+    let value = e.target.value;
     if (key === 'complete') {
       value = !item.complete
     }
     if (item[key] === value) return;
     item[key] = value;
-    this.context.itemChange(item)
+    itemChange(item)
   }
 }
